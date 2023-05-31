46fb71be9fa63523e51f9526a5a3acfd16b96983
Mahmoud Elshall <54082956+mahmoudelshall@users.noreply.github.com>
2023-06-01 02:16:53 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -26,7 +26,14 @@ function write() {
                               <th>${counter}</th>
                               <td>${tasks[key].name}</td>
                               <td> ${allPriority[tasks[key].priority]}</td>
-                              <td><i class="bi bi-trash fs-4 text-danger delete" data-id=${key} onclick="deleteTask(${key},this)"></i></td>
+                              <td>
+                              <div class = "d-flex">
+                                 <i class="bi bi-trash fs-4  text-danger delete" data-id=${key} onclick="deleteTask(${key},this)"></i>
+                                 <i class="bi bi-pencil-square fs-4 ms-1 text-primary edit" data-id=${key} onclick="editTask(${key},this)"></i>
+                                 <button class="btn btn-primary hidden" onclick = "updateTask(${key},this)">Save</button>
+                                 <button class="btn btn-danger ms-1 hidden" onclick="cancelEdit()">Cancel</button>
+                                 </div>
+                              </td>
                           </tr> `;
         counter++;
     }
@@ -40,6 +47,7 @@ window.onload = function () {
 };
 function addNew() {
     if (taskName.value == "" || taskPriority.value == "") {
+        alert("Invalid Task Data!!")
         return;
     }
     let newTask = {
@@ -59,7 +67,49 @@ add.onclick = addNew;
 
 //console.log( document.getElementsByClassName('delete'));
 function deleteTask(key, el) {
-    console.log("test", key, el);
     tasks.splice(key, 1);
+    write();
+}
+let editTask = function (key, el) {
+    let deleteBtn = el.previousElementSibling;
+    let saveBtn = el.nextElementSibling;
+    let cancelBtn = saveBtn.nextElementSibling;
+    let priorityTd = el.parentElement.parentElement.previousElementSibling;
+    let nameTd = priorityTd.previousElementSibling;
+   
+    el.classList.add('hidden');
+    deleteBtn.classList.add('hidden');
+    saveBtn.classList.remove('hidden');
+    cancelBtn.classList.remove('hidden');
+    nameTd.innerHTML = `<input class="form-control" value="${tasks[key].name}" />`
+
+    let options = ''
+    for (i in allPriority) {
+
+        options += `<option ${i == tasks[key].priority ? 'selected' : ''} value="${i}">${allPriority[i]}</option>`
+    }
+
+    priorityTd.innerHTML = `<select class="form-select">${options}</select>`
+}
+
+
+let cancelEdit = function () {
+    write();
+}
+let updateTask = function (key, el) {
+    // let deleteBtn = el.previousElementSibling;
+    // let saveBtn = el.nextElementSibling;
+    // let cancelBtn = saveBtn.nextElementSibling;
+    let taskPriority = el.parentElement.parentElement.previousElementSibling.firstElementChild;
+    let taskName = el.parentElement.parentElement.previousElementSibling.previousElementSibling.firstElementChild;
+    if (taskName.value == "" || taskPriority.value == "") {
+        alert("Invalid Task Data!!")
+        return;
+    }
+    tasks[key].name = taskName.value;
+    tasks[key].priority = taskPriority.value;
+    tasks.sort((a, b) =>
+        a.priority < b.priority ? 1 : b.priority < a.priority ? -1 : 0
+    );
     write();
 }
